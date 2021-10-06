2834485367ccec7a8b905d45cad0b21ee0a1e97c
santiadlv <49079265+santiadlv@users.noreply.github.com>
2021-10-06 17:40:48 -0600

    Challenge 3

--- src/containers/ProductDetail/index.tsx
+++ src/containers/ProductDetail/index.tsx
@@ -81,9 +81,18 @@ class ProductDetail extends React.Component<{}, ProductState> {
 
         console.log("selectedColor: " + value);
 
+        const helper = this.state.helper;
+        const sizes = helper.getSizes(value);
+
         this.setState({
-            selectedColor: value
+            selectedColor: value, 
+            sizes
         })
+
+        if (sizes.length >= 1) {
+            const defaultSelectedSize = sizes[0];
+            this.setState({selectedSize: defaultSelectedSize});
+        }
     } 
 
     changedSize = (event: any) => {
